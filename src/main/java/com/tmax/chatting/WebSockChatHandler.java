ddc149985a3198dd0cd21dbfcab4844124380cc9
unknown <jaeseung_choun@tmax.co.kr>
2022-12-01 16:26:02 +0900

    feat: dto, 비즈니스 로직 추가

--- src/main/java/com/tmax/chatting/WebSockChatHandler.java
+++ src/main/java/com/tmax/chatting/WebSockChatHandler.java
@@ -1,5 +1,7 @@
 package com.tmax.chatting;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -7,13 +9,19 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class WebSockChatHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-        TextMessage textMessage = new TextMessage("Welcome chatting sever~^^");
-        session.sendMessage(textMessage);
+//        TextMessage textMessage = new TextMessage("Welcome chatting sever~^^");
+//        session.sendMessage(textMessage);
+        ChatMessage chatMessage= objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room= chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
     }
 }
