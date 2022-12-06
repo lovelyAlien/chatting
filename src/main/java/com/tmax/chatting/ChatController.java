5d8782c7cbb460aace9c265f048ffb11ea608b1f
unknown <jaeseung_choun@tmax.co.kr>
2022-12-06 12:18:04 +0900

    feat: 발행된 메시지를 redis로 발행

--- src/main/java/com/tmax/chatting/ChatController.java
+++ src/main/java/com/tmax/chatting/ChatController.java
@@ -14,13 +14,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
-    private final SimpMessageSendingOperations messagingTemplate;
-
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
