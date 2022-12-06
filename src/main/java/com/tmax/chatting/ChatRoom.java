b29c79ae3ef65bceed475b893d0dc7f16b2de86b
unknown <jaeseung_choun@tmax.co.kr>
2022-12-06 12:19:45 +0900

    feat: pub/sub 구조로 ChatRoom이 세션 관리하지 않아도 된다.

--- src/main/java/com/tmax/chatting/ChatRoom.java
+++ src/main/java/com/tmax/chatting/ChatRoom.java
@@ -5,22 +5,23 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
-//    private Set<WebSocketSession> sessions = new HashSet<>();
 
-    @Builder
     public static ChatRoom create(String name) {
-        ChatRoom chatRoom= new ChatRoom();
-        chatRoom.roomId= UUID.randomUUID().toString();
-        chatRoom.name=name;
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
         return chatRoom;
     }
 }
